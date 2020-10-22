70e24d2bb40e11fb22ea4d7cddf39cf79d27a1f9
peggychenpw <ya5471ya@gmail.com>
2020-10-22 11:57:25 +0800

    shaffle and search

--- src/App.js
+++ src/App.js
@@ -8,27 +8,38 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      cardData: [],
-      searchField: ""
+      cardData: CardData,
+      searchField: "",
+      shuffleArr: []
     }
   }
 
-  componentDidMount() {
-    this.setState({
-      cardData: CardData
-    })
+
+  getRandomThree = () => {
+    let randomArr = [];
+    while (randomArr.length < 3) {
+      let r = Math.floor(Math.random() * this.state.cardData.length);
+      if (randomArr.indexOf(r) === -1) randomArr.push(r);
+    }
+    this.setState({ shuffleArr: randomArr })
+    // console.log(randomArr)
   }
 
-
   render() {
-    const { cardData, searchField } = this.state
+    const { cardData, searchField, shuffleArr } = this.state
     const filteredCard = cardData.filter((card) =>
       card.name.toLowerCase().includes(searchField.toLowerCase())
     );
+    const randomCard = cardData.filter((card) => card.number === String(shuffleArr[0]) || card.number === String(shuffleArr[1]) || card.number === String(shuffleArr[2]))
     return (
       <div className="App">
         <div className="header">
           <div className="tarotpedia">Tarotpedia</div>
+        </div>
+        <div className="shuffle-box">
+          <div className="shuffle-title">Close your eyes, reflect, ask yourself a question. Then click the button to get your cards.</div>
+          <button className='shuffle-button' onClick={() => this.getRandomThree()}>GET YOUR CARDS</button>
+          <CardList cardData={randomCard} />
         </div>
         <SearchField
           placeholder='Search for Cards'
